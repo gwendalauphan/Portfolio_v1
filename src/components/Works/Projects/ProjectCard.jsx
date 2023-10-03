6b3e728e5c7075b12031a067cc2e5baa4f06bdac
gwenilapeuf <gwenauphan@eisti.eu>
2023-10-03 03:13:37 +0200

    finishing project page

--- src/components/Works/Projects/ProjectCard.jsx
+++ src/components/Works/Projects/ProjectCard.jsx
@@ -1,88 +1,172 @@
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
-import Tilt from 'react-parallax-tilt';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import Tilt from "react-parallax-tilt";
 import { fadeIn, textVariant } from "../../../utils/motion";
 
 import { github } from "../../../assets";
 
-const ProjectCard = ({ index, name, description, tags, media, source_code_link }) => {
-  const [isHovered, setIsHovered] = useState(false);
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  media,
+  source_code_link,
+}) => {
+  const [isHoveredImage, setIsHoveredImage] = useState(false);
+  const [isHoveredCard, setIsHoveredCard] = useState(false);
 
   return (
-    <motion.div 
-      variants={fadeIn("up", "spring", index * 0.5, 0.75)}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full '
+    <div className="inner-div " style={{ opacity: isHoveredCard ? 1 : 0.95 }}>
+      <motion.div
+        animate={{
+          scale: isHoveredCard ? 1.05 : 1,
+        }} // <-- Ici, nous changeons l'échelle
+        transition={{ type: "spring", stiffness: 100 }}
+        className={`card-container-project relative rounded-xl ${
+          isHoveredCard ? "shadow-card-project" : ""
+        }`}
+        variants={fadeIn("up", "spring", index * 0.75, 1)}
+        onMouseEnter={() => setIsHoveredCard(true)}
+        onMouseLeave={() => setIsHoveredCard(false)}
       >
+        <Tilt
+          tiltEnable={false}
+          className="bg-black-200 p-5 rounded-2xl sm:w-[360px] w-full  "
+        >
+          <div className="relative w-full ">
+            {/* Ajout de la couche d'assombrissement */}
 
-        <div className='relative w-full'>
+            <motion.div
+              animate={{ opacity: isHoveredCard ? 1 : 0.75 }} // rend plus clair lors du survol
+              transition={{ duration: 0.2 }}
+              className="media-container relative w-full cursor-pointer"
+              onMouseEnter={() => setIsHoveredImage(true)}
+              onMouseLeave={() => setIsHoveredImage(false)}
+              onClick={() => console.log("Show project clicked")}
+            >
+              <motion.video
+                preload="none"
+                src={media.video}
+                autoPlay
+                loop
+                muted
+                playsInline
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: isHoveredImage ? 1 : 0,
+                  scale: isHoveredCard ? 1.07 : 1,
+                }}
+                transition={{
+                  duration: 1,
+                  scale: { type: "spring", stiffness: 100 },
+                }}
+                className="absolute inset-0 object-cover rounded-xl "
+              />
+              <motion.img
+                src={media.image}
+                alt={name}
+                initial={{ opacity: 1, scale: 1 }} // <-- Ajout de l'échelle initiale
+                animate={{
+                  opacity: isHoveredImage ? 0 : 1,
+                  scale: isHoveredCard ? 1.07 : 1, // <-- Changement de l'échelle
+                }}
+                transition={{
+                  opacity: { duration: 0.5 },
+                  scale: { type: "spring", stiffness: 100 }, // <-- Transition pour l'échelle
+                }}
+                className=" inset-0 object-cover rounded-xl "
+              />
 
-          
-          {isHovered && media.video ? (
-            <video src={media.video} autoPlay loop muted playsInline />
-          ) : (
-            <img src={media.image} alt={name} />
-          )}
-
-          <div className='absolute top-[-40px] left-1/2 transform -translate-x-1/2'>
-              <h3 className='text-white font-bold text-[28px] text-shadow-card-project'>
+              <div className="absolute top-[-40px] left-1/2 transform -translate-x-1/2">
+                <h3 className="text-white font-bold text-[28px] text-shadow-card-project">
                   {name}
-              </h3>
+                </h3>
+              </div>
+
+              <div className="absolute left-[260px] top-[-5px] left-3/4 transform -translate-x-1/10  m-3  ">
+                <div
+                  onClick={() => window.open(source_code_link, "_blank")}
+                  className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                >
+                  <img
+                    src={github}
+                    alt="source code"
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
+              </div>
+            </motion.div>
           </div>
 
-
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-            <div
-                onClick={() => window.open(source_code_link, "_blank")}
-                className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-              >
-                <img
-                  src={github}
-                  alt='source code'
-                  className='w-1/2 h-1/2 object-contain'
-                />
+          <motion.div
+            animate={{ opacity: isHoveredCard ? 1 : 0.8 }} // rend plus clair lors du survol
+            transition={{ duration: 0.1 }}
+          >
+            <div className="mt-5">
+              <p className="mt-2 text-secondary text-[14px]">{description}</p>
             </div>
-          </div>
-        </div>
 
-        <div className='mt-5'>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-        </div>
-
-        <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.technos.map((tag, index) => (
-              <p
+            <div className="mt-3 flex flex-wrap gap-2">
+              {tags.technos.map((tag, index) => (
+                <p
                   key={`techno-${name}-${tag.name}-${index}`}
-                  className={`text-[14px] text-${tag.color}`}
+                  className={`text-[12px] text-${tag.color}`}
                   style={{ color: tag.color }}
-              >
-                  #{tag.name}
-              </p>
-          ))}
-          {tags.thèmes.map((tag, index) => (
-              <p
+                >
+                  {` #${tag.name} `}
+                  &nbsp;
+                </p>
+              ))}
+              {tags.thèmes.map((tag, index) => (
+                <p
                   key={`theme-${name}-${tag.name}-${index}`}
-                  className={`text-[14px] text-${tag.color}`}
+                  className={`text-[12px] text-${tag.color}`}
                   style={{ color: tag.color }}
+                >
+                  {` #${tag.name} `}
+                  &nbsp;
+                </p>
+              ))}
+            </div>
+
+            <div className="relative pt-2 pb-3 flex flex-col h-full">
+              {" "}
+              {/* Conteneur commun */}
+              {/* Texte par défaut */}
+              <motion.div
+                initial={{ y: 0, opacity: 1 }}
+                animate={{
+                  y: isHoveredCard ? -20 : 0,
+                  opacity: isHoveredCard ? 0 : 1,
+                }}
+                transition={{ duration: 0.3 }}
+                className="absolute text-white text-[16px] "
               >
-                  #{tag.name}
-              </p>
-          ))}
-      </div>
-
-
-        
-        {/* ... autres contenus de la carte ... */}
-      </Tilt>
-    </motion.div>
+                balblabla
+              </motion.div>
+              {/* Texte au survol */}
+              <motion.div
+                initial={{ y: 20, opacity: 0 }}
+                animate={{
+                  y: isHoveredCard ? 0 : 20,
+                  opacity: isHoveredCard ? 1 : 0,
+                }}
+                transition={{ duration: 0.3 }}
+                className="absolute flex items-center space-x-2 text-white text-[16px]"
+                onClick={() => console.log("Show project clicked")}
+              >
+                <span className="text-white hover:text-custom-green cursor-pointer">
+                  Show project
+                </span>
+                <div className="w-8 h-[1px] bg-white"></div>
+              </motion.div>
+            </div>
+          </motion.div>
+          {/* ... autres contenus de la carte ... */}
+        </Tilt>
+      </motion.div>
+    </div>
   );
 };
 
--- src/components/Works/Projects/Projects.jsx
+++ src/components/Works/Projects/Projects.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import ProjectCard from './ProjectCard';
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
 import { projects } from "../../../constants";
 import { motion } from "framer-motion";
 import { styles } from "../../../styles";
@@ -7,33 +7,35 @@ import SectionWrapper from "../../../hoc/SectionWrapper";
 
 import { textVariant } from "../../../utils/motion";
 
-
 function Projects() {
-
-
   return (
     <>
-
-    <motion.div className="mb-4" variants={textVariant()}>
-    <p className={`${styles.sectionSubText} keyword-blue`}>My Opus</p>
-    <h2 className={`${styles.heroHeadText} text-white Home-Title-text-shadow`}><span className="keyword-purple">Projects.</span></h2>
-    </motion.div>
-    <div className="inner-div" style={{flex: 1}}>
-    <pre className="code-snippet mt-6 " style={{ backgroundColor: 'rgba(0, 0, 0, 0.9)', marginBottom: "24px"}}>
-            <span className="prompt">gwendal@portfolio:~$</span> <span className="command">${'ls  ~/projects  -al'}</span>
+      <motion.div className="mb-4" variants={textVariant()}>
+        <p className={`${styles.sectionSubText} keyword-blue`}>My Opus</p>
+        <h2
+          className={`${styles.heroHeadText} text-white Home-Title-text-shadow`}
+        >
+          <span className="keyword-purple">Projects.</span>
+        </h2>
+      </motion.div>
+      <div className="inner-div" style={{ flex: 1 }}>
+        <pre
+          className="code-snippet mt-6 "
+          style={{
+            backgroundColor: "rgba(0, 0, 0, 0.9)",
+            marginBottom: "24px",
+          }}
+        >
+          <span className="prompt">gwendal@portfolio:~$</span>{" "}
+          <span className="command">${"ls  ~/projects  -al"}</span>
         </pre>
-    </div>
+      </div>
 
-    <div className='mt-20 flex flex-wrap gap-7'>
-      {projects.map((project, index) => (
-        <ProjectCard 
-        key={`project-${index}`} 
-        index={index} 
-        {...project} />
-      ))}
-
-    </div>
-
+      <div className="mt-20 flex flex-wrap gap-10">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
     </>
   );
 }
